3c960f38ef3ad49ea8f667a28a9d7e5ae793a81b
brettratner <ratnerb1@tcnj.edu>
2016-03-08 12:25:55 -0500

    First Commit
    added the switch to mini game and back to main game

---
+++ Soul_Searcher/Assets/Scripts/Player.cs
@@ -0,0 +1,150 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;	//Allows us to use UI.
+
+namespace Completed
+{
+	//Player inherits from MovingObject, our base class for objects that can move, Enemy also inherits from this.
+	public class Player : MovingObject
+	{
+		public float restartLevelDelay = 1f;		//Delay time in seconds to restart level.
+		public int wallDamage = 1;					//How much damage a player does to a wall when chopping it.
+		public AudioClip moveSound1;				//1 of 2 Audio clips to play when player moves.
+		public AudioClip moveSound2;				//2 of 2 Audio clips to play when player moves.
+		public AudioClip gameOverSound;				//Audio clip to play when player dies.
+		public AudioClip drinkSound1;				//1 of 2 Audio clips to play when player collects a soda object.
+		public AudioClip drinkSound2;				//2 of 2 Audio clips to play when player collects a soda object.
+		
+		private Animator animator;					//Used to store a reference to the Player's animator component.						//Used to store player food points total during level.
+		
+		
+		//Start overrides the Start function of MovingObject
+		protected override void Start ()
+		{
+			//Get a component reference to the Player's animator component
+			animator = GetComponent<Animator>();
+			
+			//Call the Start function of the MovingObject base class.
+			base.Start ();
+		}
+		
+		private void Update ()
+		{
+			//If it's not the player's turn, exit the function.
+			if(!GameManager.instance.playersTurn) return;
+			
+			int horizontal = 0;  	//Used to store the horizontal move direction.
+			int vertical = 0;		//Used to store the vertical move direction.
+			
+			//Check if we are running either in the Unity editor or in a standalone build.
+			#if UNITY_STANDALONE || UNITY_WEBPLAYER
+			//if(MiniGameManager.isPickedUp == false){
+			//Get input from the input manager, round it to an integer and store in horizontal to set x axis move direction
+			horizontal = (int) (Input.GetAxisRaw ("Horizontal"));
+			
+			//Get input from the input manager, round it to an integer and store in vertical to set y axis move direction
+			vertical = (int) (Input.GetAxisRaw ("Vertical"));
+			//}
+			//Check if moving horizontally, if so set vertical to zero.
+			if(horizontal != 0)
+			{
+				vertical = 0;
+			}
+			//Check if we are running on iOS, Android, Windows Phone 8 or Unity iPhone
+			#elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
+			
+			#endif //End of mobile platform dependendent compilation section started above with #elif
+			//Check if we have a non-zero value for horizontal or vertical
+			if(horizontal != 0 || vertical != 0)
+			{
+				//Call AttemptMove passing in the generic parameter Wall, since that is what Player may interact with if they encounter one (by attacking it)
+				//Pass in horizontal and vertical as parameters to specify the direction to move Player in.
+				AttemptMove<Wall> (horizontal, vertical);
+			}
+		}
+		
+		//AttemptMove overrides the AttemptMove function in the base class MovingObject
+		//AttemptMove takes a generic parameter T which for Player will be of the type Wall, it also takes integers for x and y direction to move in.
+		protected override void AttemptMove <T> (int xDir, int yDir)
+		{
+			
+			//Call the AttemptMove method of the base class, passing in the component T (in this case Wall) and x and y direction to move.
+			base.AttemptMove <T> (xDir, yDir);
+			
+			//Hit allows us to reference the result of the Linecast done in Move.
+			RaycastHit2D hit;
+			
+			//If Move returns true, meaning Player was able to move into an empty space.
+			if (Move (xDir, yDir, out hit)) 
+			{
+				//Call RandomizeSfx of SoundManager to play the move sound, passing in two audio clips to choose from.
+				SoundManager.instance.RandomizeSfx (moveSound1, moveSound2);
+			}
+			
+			//Set the playersTurn boolean of GameManager to false now that players turn is over.
+			GameManager.instance.playersTurn = false;
+		}
+		
+		
+		//OnCantMove overrides the abstract function OnCantMove in MovingObject.
+		//It takes a generic parameter T which in the case of Player is a Wall which the player can attack and destroy.
+		protected override void OnCantMove <T> (T component)
+		{
+			//Set hitWall to equal the component passed in as a parameter.
+			Wall hitWall = component as Wall;
+			
+			//Call the DamageWall function of the Wall we are hitting.
+			hitWall.DamageWall (wallDamage);
+			
+			//Set the attack trigger of the player's animation controller in order to play the player's attack animation.
+			animator.SetTrigger ("playerChop");
+		}
+		
+		
+		//OnTriggerEnter2D is sent when another object enters a trigger collider attached to this object (2D physics only).
+		private void OnTriggerEnter2D (Collider2D other)
+		{
+			//Check if the tag of the trigger collided with is Exit.
+			if(other.tag == "Exit")
+			{
+				//Invoke the Restart function to start the next level with a delay of restartLevelDelay (default 1 second).
+				Invoke ("Restart", restartLevelDelay);
+				
+				//Disable the player object since level is over.
+				enabled = false;
+			}
+			//Check if the tag of the trigger collided with is Food.
+			else if(other.tag == "Soda")
+			{
+				//Call the RandomizeSfx function of SoundManager and pass in two eating sounds to choose between to play the eating sound effect.
+				SoundManager.instance.RandomizeSfx (drinkSound1, drinkSound2);
+
+				//Disable the food object the player collided with.
+				other.gameObject.SetActive (false);
+			}
+			else if(other.tag == "Brain")
+			{
+				//Call the RandomizeSfx function of SoundManager and pass in two eating sounds to choose between to play the eating sound effect.
+				SoundManager.instance.RandomizeSfx (drinkSound1, drinkSound2);
+
+				//Disable the food object the player collided with.
+				other.gameObject.SetActive (false);
+
+				animator.SetTrigger ("BrainIdle");
+			}
+			else if(other.tag == "Soul")
+			{
+				//Call the RandomizeSfx function of SoundManager and pass in two eating sounds to choose between to play the eating sound effect.
+				SoundManager.instance.RandomizeSfx (drinkSound1, drinkSound2);
+				MiniGameManager.isPickedUp = true;
+				Debug.Log ("it is: " + MiniGameManager.isPickedUp);
+				//Disable the food object the player collided with.
+				other.gameObject.SetActive (false);
+
+				animator.SetTrigger ("Spirit");
+			}
+		}
+
+	}
+}
+
